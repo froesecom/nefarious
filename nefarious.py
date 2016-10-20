159fb826408fe167a94da905d1efff7b590f2a77
Erik Froese <froesecom@gmail.com>
2016-10-20 22:35:09 +1100

    got me a little attribute name

--- nefarious.py
+++ nefarious.py
@@ -5,7 +5,7 @@ import time
 print "just imported selenium"
 baseurl = ""
 el_id = ""
-mydriver = webdriver.Chrome()
-mydriver.get(baseurl)
-el = mydriver.get_element_by_id(el_id)
-print el
+driver = webdriver.Chrome("/Library/drivers/chromedriver")
+driver.get(baseurl)
+el = driver.find_element_by_id(el_id)
+print el.get_attribute("name")
