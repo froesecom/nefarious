045e335bc2f63d26e19cbed229aa0e4aa5ee3a59
Erik Froese <froesecom@gmail.com>
2016-10-20 22:03:04 +1100

    add config dir and some test selium code

--- nefarious.py
+++ nefarious.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 import time
 
 print "just imported selenium"
+baseurl = ""
+el_id = ""
+mydriver = webdriver.Chrome()
+mydriver.get(baseurl)
+el = mydriver.get_element_by_id(el_id)
+print el
