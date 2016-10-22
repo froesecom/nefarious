86a2e466e2af81252b09af9518d93ce286cdc766
Erik Froese <froesecom@gmail.com>
2016-10-22 19:59:27 +1100

    read csv and print output of a column

--- nefarious.py
+++ nefarious.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 import yaml
+import csv
 
 print "just imported selenium"
 stream   = file("config/settings.yml", "r")
@@ -8,7 +9,14 @@ settings = yaml.load(stream)
 
 baseurl = settings["baseurl"]
 el_id = settings["input"]["id"]
+
+with open("config/input_data.csv", "rU") as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    print(row["ABN"]) #row header is ABN in this case
+
 #driver = webdriver.Chrome("/Library/drivers/chromedriver")
+
 #driver.get(baseurl)
 #el = driver.find_element_by_id(el_id)
 #print el.get_attribute("name")
