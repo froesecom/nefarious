98f68e923db43a8838bcc7366ff59af8f06040d9
Erik Froese <froesecom@gmail.com>
2016-10-22 19:28:18 +1100

    add yaml parsing

--- nefarious.py
+++ nefarious.py
@@ -1,11 +1,14 @@
-
 from selenium import webdriver
 import time
+import yaml
 
 print "just imported selenium"
-baseurl = ""
-el_id = ""
-driver = webdriver.Chrome("/Library/drivers/chromedriver")
-driver.get(baseurl)
-el = driver.find_element_by_id(el_id)
-print el.get_attribute("name")
+stream   = file("config/settings.yml", "r")
+settings = yaml.load(stream)
+
+baseurl = settings["baseurl"]
+el_id = settings["input"]["id"]
+#driver = webdriver.Chrome("/Library/drivers/chromedriver")
+#driver.get(baseurl)
+#el = driver.find_element_by_id(el_id)
+#print el.get_attribute("name")
