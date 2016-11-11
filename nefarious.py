b0b53af7707a8b7ca3c629c4b42cb7ff52d3504e
Erik Froese <froesecom@gmail.com>
2016-11-11 22:36:14 +1100

    iterate over .csv and import debugger

--- nefarious.py
+++ nefarious.py
@@ -2,21 +2,28 @@ from selenium import webdriver
 import time
 import yaml
 import csv
+import pdb
 
 print "just imported selenium"
 stream   = file("config/settings.yml", "r")
 settings = yaml.load(stream)
 
 baseurl = settings["baseurl"]
-el_id = settings["input"]["id"]
+driver = webdriver.Chrome("/Library/drivers/chromedriver")
 
-with open("config/input_data.csv", "rU") as csvfile:
+print "opening csv..."
+with open("config/input_data_test.csv", "rU") as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
     print(row["ABN"]) #row header is ABN in this case
-
-#driver = webdriver.Chrome("/Library/drivers/chromedriver")
+     
+    #pdb.set_trace()
+    for instruction in settings['actions']:
+      print("hello?")
+      #if key == "input":
+     #   el = driver.find_element_by_id() 
+        
 
 #driver.get(baseurl)
 #el = driver.find_element_by_id(el_id)
-#print el.get_attribute("name")
+#print   .get_attribute("name")
